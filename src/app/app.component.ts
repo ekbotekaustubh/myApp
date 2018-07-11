2cb9c593917bf9d2c134be1ad65b578bd6a057cb
Kaustubh Ekbote <kaustubh51@gmal.com>
2018-07-11 23:19:58 +0530

    Hero demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My first angular app';
+  title = 'Tour of Heroes';
 }
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
